157f78498638652113e9394b3307fd4bfa04eac1
KhushiSingh61 <khushi09123@gmial.com>
2026-01-15 21:34:46 +0530

    Added EachPenPerstudent

---
+++ basic-programs/EachPenPerStudent.java
@@ -0,0 +1,7 @@
+public class EachPenPerStudent{
+    public static void main(String args[]){
+        int PenPerStudent = 14/3;
+        int PenRemaining = 14%3;
+        System.out.println("Pen per student is "+PenPerStudent+ " and the remaining pen not distributed is "+PenRemaining);
+    }
+    }
