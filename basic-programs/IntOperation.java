5aef7716d19b25d4423cd0f97c7e7eb91d41f717
KhushiSingh61 <khushi09123@gmial.com>
2026-01-15 22:06:26 +0530

    Added IntOperation

---
+++ basic-programs/IntOperation.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class IntOperation{
+public static void main(String args[]){
+Scanner sc = new Scanner(System.in);
+int a,b,c;
+a=sc.nextInt();
+b=sc.nextInt();
+c=sc.nextInt();
+int result1 = a+b*c;
+int result2 = a*b+c;
+int result3 = c+a/b;
+int result4 = a%b+c;
+System.out.println("The result of IntOperations are "+result1+" , "+result2+" , "+result3+" , "+result4);
+}
+}
