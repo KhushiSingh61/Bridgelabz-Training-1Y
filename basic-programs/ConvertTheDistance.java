7798a9c45789b8f30207e87ec6cca4ba9e0c3cf3
KhushiSingh61 <khushi09123@gmial.com>
2026-01-15 21:29:45 +0530

    Added ConvertTheDistance

---
+++ basic-programs/ConvertTheDistance.java
@@ -0,0 +1,7 @@
+public class ConvertTheDistance{
+    public static void main(String args[]){
+        double distance = 10.8;
+        double miles = 10.8*1.6;
+        System.out.println("The Distance 10km in miles is " +miles);
+    }
+    }
