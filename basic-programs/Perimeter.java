402fccc7033339e3afdac3b5132c432886ca89f7
KhushiSingh61 <khushi09123@gmial.com>
2026-01-15 21:38:22 +0530

    Added Perimeter

---
+++ basic-programs/Perimeter.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Perimeter{
+public static void main(String args[]){
+Scanner sc = new Scanner(System.in);
+double PerimeterOfSquare;
+PerimeterOfSquare=sc.nextDouble();
+double side = PerimeterOfSquare/4;
+
+
+System.out.println("The length of side is  " +side+" whose perimeter is "+PerimeterOfSquare);
+}
+}
