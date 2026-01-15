982014458bfb58825daa787a5c81f87352b299b1
KhushiSingh61 <khushi09123@gmial.com>
2026-01-15 21:26:38 +0530

    New program

---
+++ basic-programs/AveragePercentMark.java
@@ -0,0 +1,13 @@
+public class AveragePercentMark{
+public static void main(String args[]){
+m1();
+}
+public static void m1(){
+    int x=94;
+    int y=95;
+    int z=96;
+    double avg = (x+y+z)/3;
+    System.out.println("Sam's average marks in PCM is "+avg);
+
+}
+}
