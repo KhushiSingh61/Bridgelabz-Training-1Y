f978fed4c61ed0d67448591ca10a06396e9d2ab7
KhushiSingh61 <khushi09123@gmial.com>
2026-01-15 21:39:57 +0530

    Added ProfitAndLoss

---
+++ basic-programs/ProfitAndLoss.java
@@ -0,0 +1,9 @@
+public class ProfitAndLoss{
+    public static void main(String args[]){
+        int CP=129;
+        int SP=191;
+        int profit = SP - CP;
+        int profit_percentage = (profit/CP)*100;
+        System.out.println("The Cost Price is INR 129 and Selling Price is INR 191"+"The profit is INR"+profit+"and profit percentage is"+profit_percentage+"%");
+    }
+}
