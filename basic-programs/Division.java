260a8620f8416f79f0be8f39a634199baec5fdc0
KhushiSingh61 <khushi09123@gmial.com>
2026-01-15 21:59:13 +0530

    Added Division

---
+++ basic-programs/Division.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Division{
+public static void main(String args[]){
+Scanner sc = new Scanner(System.in);
+double number1,number2;
+number1=sc.nextDouble();
+number2=sc.nextDouble();
+double quotient = number1/number2;
+double remainder = number1%number2;
+System.out.println("The Quotient is "+quotient+ " remainder is "+remainder+" of two number "+number1+ " and "+number2);
+
+}
+}
