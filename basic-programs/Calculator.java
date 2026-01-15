1d75901902bf1a6d6dc08a2e29376435029951cd
KhushiSingh61 <khushi09123@gmial.com>
2026-01-15 21:43:02 +0530

    Added Calculator

---
+++ basic-programs/Calculator.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Calculator{
+public static void main(String args[]){
+Scanner sc = new Scanner(System.in);
+float number1,number2;
+number1=sc.nextFloat();
+number2 = sc.nextFloat();
+
+System.out.println("Addition = " + (number1 + number2));
+System.out.println("Subtraction = " + (number1 - number2));
+System.out.println("Multiplication = " + (number1 * number2));
+System.out.println("Division = " + (number1 / number2));
+
+}
+}
