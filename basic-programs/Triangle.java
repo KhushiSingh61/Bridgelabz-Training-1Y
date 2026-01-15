bd913ec10e7395c50fc99663b50a40f3b00a32ce
KhushiSingh61 <khushi09123@gmial.com>
2026-01-15 21:40:41 +0530

    Added Triangle

---
+++ basic-programs/Triangle.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Triangle{
+public static void main(String args[]){
+Scanner sc = new Scanner(System.in);
+double Height,base;
+Height=sc.nextDouble();
+base=sc.nextDouble();
+double area = 0.5*base*Height;
+
+System.out.println("Area of Triangle is " +area);
+}
+}
+ 
