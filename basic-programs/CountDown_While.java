cb1b72f8ee549099daf9695dc1e425de0ce83cf4
KhushiSingh61 <khushi09123@gmial.com>
2026-01-25 22:09:13 +0530

    Added CountDown_While.java

---
+++ basic-programs/CountDown_While.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class CountDown_While{
+       public static void main(String [] args){
+	   
+	   Scanner sc = new Scanner(System.in);
+	   
+	   int n1 = sc.nextInt();
+	  
+	  while(n1>=1){
+	  System.out.println(n1);
+	  n1--;
+	   
+	   }
+}
+}
