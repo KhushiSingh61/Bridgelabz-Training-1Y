dc2dd965b6a9acb85aac7abed4db9d2ae6bee5dd
KhushiSingh61 <khushi09123@gmial.com>
2026-01-26 22:15:13 +0530

    SumUntilZeroAndNegative.java

---
+++ basic-programs/SumUntilZeroAndNegative.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class SumUntilZeroAndNegative{
+       public static void main(String [] args){
+	   
+	   Scanner sc = new Scanner(System.in);
+	   
+	   
+	   double n1=sc.nextDouble();;
+	   double sum =0;
+	  
+	 
+	   while(n1 !=0 && n1>0){   
+		  sum = sum + n1;
+		  
+		  System.out.println("Enter number again for the sum : ");
+	     
+ 		  n1 = sc.nextDouble();
+	
+	   
+	  }
+	  
+	  System.out.println("Total sum until zero or negative number is " + sum);
+ }
+}
