5b4081ec8d5edb87630790205e1a6e6ddc7c50c3
KhushiSingh61 <khushi09123@gmial.com>
2026-02-04 23:43:59 +0530

    Added

---
+++ src/com/gla/arrays/VotingCheck.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class VotingCheck{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int[] age = new int[10];
+		System.out.println("Enter age : ");
+		for(int i=0;i<age.length;i++){
+			age[i] = sc.nextInt();
+	}
+	for(int i=0;i<age.length;i++){
+		if(age[i]<0){
+			System.out.println("Invalid Age");
+		}else if(age[i]<=18){
+			System.out.println("Can not vote");
+		}else{
+			System.out.println("Can Vote");
+		}
+	}
+	}
+}
