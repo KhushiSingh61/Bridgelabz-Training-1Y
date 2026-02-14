8cb03d1eccc934c7a029ae3f47f1646e0f36e9f4
KhushiSingh61 <khushi09123@gmial.com>
2026-02-14 23:04:03 +0530

    Added SumOfNaturalNumbers.java

---
+++ SumOfNaturalNumbers.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class SumOfNaturalNumbers {
+    public static int findSum(int n) {
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum = sum + i;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number (n): ");
+        int n = sc.nextInt();
+        int result = findSum(n);
+        System.out.println("Sum of first " + n + " natural numbers is: " + result);
+    }
+}
